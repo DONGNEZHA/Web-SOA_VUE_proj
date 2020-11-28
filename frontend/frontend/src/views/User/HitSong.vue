baefc1320a9d7796e317d8a8fab1ad3ce5f0f556
Cecilia <44920463+DONGNEZHA@users.noreply.github.com>
2020-11-28 16:55:13 +0800

    框架

---
+++ frontend/frontend/src/views/User/HitSong.vue
@@ -0,0 +1,11 @@
+<template>
+      <p>这里是歌单电台页面，需要填充基于网易云的歌单电台功能</p>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
---
+++ frontend/frontend/src/views/User/Singer.vue
@@ -0,0 +1,11 @@
+<template>
+      <p>这里是歌手页面，需要填充基于网易云的歌手功能</p>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
